05ae7f55ac7e284c2510d385cc994b681ae30dfd
vinhub <vin@introp.net>
2019-11-07 18:27:11 -0800

    collision detection with landmark

---
+++ Assets/LandmarkHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LandmarkHandler : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "ColliderBody" || other.name == "ColliderFront" || other.name == "ColliderBottom")
+        {
+            Debug.Log("Car crossed landmark " + this.name);
+        }
+    }
+}
