66a839bbf8c54f1f69715cf5d8a21927f83cdd77
Vinayak (Vin) Bhalerao <vin.bhalerao@gmail.com>
2020-02-28 11:41:19 -0800

    script to reset playerprefs

---
+++ Assets/resetprefs.sh
@@ -0,0 +1,6 @@
+﻿#!/bin/bash
+echo "Resetting PlayerPrefs:"
+plutil -p ~/Library/Preferences/unity.RedmondLabs.MapGenius.plist
+defaults write ~/Library/Preferences/unity.RedmondLabs.MapGenius HideInstructionsAtStart -int 0
+plutil -p ~/Library/Preferences/unity.RedmondLabs.MapGenius.plist
+echo "Done."
