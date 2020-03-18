bdc5b92a56f4a1f259c0144475581144b5fe3a6e
vinhub <vin@introp.net>
2020-03-18 15:10:14 -0700

    bug fix in on track handling

--- Assets/Scripts/GameSystem.cs
+++ Assets/Scripts/GameSystem.cs
@@ -564,6 +564,7 @@ public class GameSystem : MonoBehaviour
             {
                 roadNext = road;
                 iOrigPointNext = iOrigPoint;
+                dist = distNext;
                 continue;
             }
         }
