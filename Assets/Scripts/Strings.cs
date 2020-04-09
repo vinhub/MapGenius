4bcaaba6b34b3fdbf351b59e95cc3bdb7a743e6c
vinhub <vin@introp.net>
2020-04-09 13:44:52 -0700

    game lost message updated

--- Assets/Scripts/Strings.cs
+++ Assets/Scripts/Strings.cs
@@ -86,7 +86,7 @@
     public const string FirstLandmarkCrossedMessageFormat = "You just crossed the  <i><color=#00ff00ff>\"{0}\"</color></i>  landmark!\r\n\r\nWe will now demonstrate how to mark its position on the map.";
     public const string OtherLandmarkCrossedMessageFormat = "You just crossed the  <i><color=#00ff00ff>\"{0}\"</color></i>  landmark!\r\n\r\nDrag and drop the blinking marker to the appropriate position on the map.";
     public const string LevelWonMessageFormat = "<b><color=#00ff00ff>Congratulations! You won!</color></b>\r\n\r\nYou scored {0}/{1} points in {2} seconds!";
-    public const string LevelLostMessageFormat = "Map completed, but with some mistakes.\r\n\r\nYou scored {0}/{1} points in {2} seconds.";
+    public const string LevelLostMessageFormat = "<b><color=#ff0000ff>Map completed, but with some mistakes.</color></b>\r\n\r\nYou scored {0}/{1} points in {2} seconds.";
     public const string CarSpeedStatusFormat = "Speed: {0:F0}";
     public const string CarRevsStatusFormat = "Revs: {0:d3}";
     public const string GetBackOnTrackMessage = "Press T to get back on track.";
