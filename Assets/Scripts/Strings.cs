da451b10c4d3a0bdf175bedcdfa6a89d1c1990e5
vinhub <vin@introp.net>
2020-09-11 14:58:54 -0700

    improved game over message

--- Assets/Scripts/Strings.cs
+++ Assets/Scripts/Strings.cs
@@ -108,5 +108,5 @@
     public const string VictoryLapInfoMessage = "Sit back and relax! You are on auto-drive until the end of the victory lap.";
     public const string VictoryLapStartingMessage = "It's time to take a victory lap!\r\n\r\nJust sit back, relax, and let us drive you around for a bit. You'll get controls back at the end of the celebration.";
     public const string FreeDriveMessage = "Drive freely anywhere you want to, including offroad. Hit 'Esc' when bored.";
-    public const string GameOverMessage = "<b><color=#00ff00ff>Game Over!\r\n\r\nBye!</color></b>";
+    public const string GameOverMessage = "<b><color=#00ff00ff>Congratulations! You're a MapGenius!\r\n\r\nThanks for playing. Bye!</color></b>";
 }
